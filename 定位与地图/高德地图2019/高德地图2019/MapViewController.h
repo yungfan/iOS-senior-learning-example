5b11e4bd85016697e36723eb5057c952cda8f582
杨帆 <yangfanvw@qq.com>
2019-04-28 17:29:50 +0800

    新增高德地图的定位与地图基本使用

---
+++ 定位与地图/高德地图2019/高德地图2019/MapViewController.h
@@ -0,0 +1,17 @@
+//
+//  MapViewController.h
+//  高德地图2019
+//
+//  Created by student on 2019/4/28.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 定位与地图/高德地图2019/高德地图2019/MapViewController.m
@@ -0,0 +1,32 @@
+//
+//  MapViewController.m
+//  高德地图2019
+//
+//  Created by student on 2019/4/28.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
