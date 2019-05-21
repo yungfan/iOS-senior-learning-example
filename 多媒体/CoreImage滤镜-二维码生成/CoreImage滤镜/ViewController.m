075605a199fbf1171852f71167e4869c998f351b
杨帆 <yangfanvw@qq.com>
2019-05-21 20:35:34 +0800

    更新CoreImage代码

--- 多媒体/CoreImage滤镜-二维码生成/CoreImage滤镜/ViewController.m
+++ 多媒体/CoreImage滤镜-二维码生成/CoreImage滤镜/ViewController.m
@@ -21,73 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-   
-    
-    //利用二维码滤镜来生成二维码
+    // 1. 实例化二维码滤镜
     CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
-    
+    // 2. 恢复滤镜的默认属性
     [filter setDefaults];
     
-    //二维码中存储的数据
-    NSString *str = @"https://www.baidu.com";
+    // 3. 将字符串转换成NSData
+    NSData *data = [@"www.abc.edu.cn" dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
-    
-    //将数据经过滤镜处理，通过KVC设置滤镜inputMessage数:通过间接的方式给类的属性赋值
+    // 4. 通过KVC设置滤镜inputMessage数据
     [filter setValue:data forKey:@"inputMessage"];
     
-    //KVC应用
-    [self.inputPwd setValue:[UIColor redColor] forKeyPath:@"_placeholderLabel.textColor"];
+    // 5. 获得滤镜输出的图像
+    CIImage *outputImage = [filter outputImage];
     
+    // 5.1 对CIImage进行转换 使得二维码变清晰
+    outputImage = [outputImage imageByApplyingTransform:CGAffineTransformMakeScale(5, 5)];
     
-    //滤镜将输入数据转成一张二维码图片
+    // 6. 将CIImage转换成UIImage
+    UIImage *image =  [UIImage imageWithCIImage:outputImage scale:20.0 orientation:UIImageOrientationUp];
     
-    CIImage *outputImage = filter.outputImage;
-    
-    
-    //通过位图创建高清图片
-    UIImage *resizedImage = [self resizeImage:[UIImage imageWithCIImage:outputImage]
-                             
-                                  withQuality:kCGInterpolationNone
-                             
-                                         rate:10.0];
-    
-    self.qrcodeImg.image = resizedImage;
+    self.qrcodeImg.image = image;
     
 }
-
-
-
-//iOS绘画
-- (UIImage *)resizeImage:(UIImage *)image
-
-             withQuality:(CGInterpolationQuality)quality
-
-                    rate:(CGFloat)rate {
-    
-    UIImage *resizedImg = nil;
-    
-    CGFloat width = image.size.width * rate;
-    
-    CGFloat height = image.size.height * rate;
-    
-    UIGraphicsBeginImageContext(CGSizeMake(width, height));
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextSetInterpolationQuality(context, quality);
-    
-    [image drawInRect:CGRectMake(0, 0, width, height)];
-    
-    resizedImg = UIGraphicsGetImageFromCurrentImageContext();
-    
-    UIGraphicsEndImageContext();
-    
-    return resizedImg;
-    
-}
-
-
 
 
 //滤镜（CIFliter）：CIFilter 产生一个CIImage。典型的，接受一到多的图片作为输入，经过一些过滤操作，产生指定输出的图片。
